6e30f7c5ede638b4f0ee762b7abf5a54f3af2bae
Leandro Marques <47747696+LeandroJerse@users.noreply.github.com>
2024-12-02 13:23:59 +0000

    Ajustes

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home(){
+    return <h1>Olá minha querida mãe, quem fala é seu filho Leandro. Este é meu primeiro site hosteado pelo meu computador e quero dedicalo a pessoa que mais amo no mundo, você! Te amo muito! </h1>
+}
+
+export default Home;
