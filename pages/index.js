99ef8081c939a4b2656249ddae8401fca39e9c90
Leandro Marques <47747696+LeandroJerse@users.noreply.github.com>
2024-12-08 17:03:50 +0000

    Adicionando scripts `lint:check`e `lint:write`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
-function Home(){
-    return <h1>Olá minha querida mãe, quem fala é seu filho Leandro. Este é meu primeiro site hosteado pelo meu computador e quero dedicalo a pessoa que mais amo no mundo, você! Te amo muito! </h1>
+function Home() {
+  return (
+    <h1>
+      Olá minha querida mãe, quem fala é seu filho Leandro. Este é meu primeiro
+      site hosteado pelo meu computador e quero dedicalo a pessoa que mais amo
+      no mundo, você! Te amo muito!{" "}
+    </h1>
+  );
 }
 
 export default Home;
